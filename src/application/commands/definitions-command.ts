793f7ac45312303ead3f44bfe2ccf558111529ee
jstenmark <johannes@stenmark.in>
2024-09-26 21:08:35 +0200

    fix: fix copyDefinitions printing [object Object]

--- src/application/commands/definitions-command.ts
+++ src/application/commands/definitions-command.ts
@@ -1,10 +1,12 @@
 import * as vscode from 'vscode'
 
-import {ClipboardManager,type ClipboardManager as TClipboardManager} from '../../infra/clipboard/clipboard-manager'
+import {codeBlock} from '../../domain/models/inquiry-template'
+import {ClipboardManager, type ClipboardManager as TClipboardManager} from '../../infra/clipboard/clipboard-manager'
+import {configStore} from '../../infra/config/config-store'
+import {getDocumentPath} from '../../infra/vscode/document'
 import {Notify} from '../../infra/vscode/notification'
-import {StatusBarManager,type StatusBarManager as TStatusBarManager } from '../../infra/vscode/statusbar-manager'
+import {StatusBarManager, type StatusBarManager as TStatusBarManager} from '../../infra/vscode/statusbar-manager'
 import {container} from '../../inversify/inversify.config'
-import {TYPES} from '../../inversify/types'
 
 
 export async function copyDefinitions(): Promise<void> {
@@ -19,8 +21,18 @@ export async function copyDefinitions(): Promise<void> {
     editor.document.uri,
   )
 
+  const filePath = configStore.get<boolean>('enablePath') ? getDocumentPath(editor) : undefined
+
+  const formattedSymbols: string[] = symbols.map(symbol => {
+    const definitionText = editor.document.getText(symbol.range)
+    const lineNumStart = symbol.range.start.line + 1
+    const lineNumEnd = symbol.range.end.line + 1
+
+    return codeBlock(definitionText, filePath, editor.document.languageId, lineNumStart, lineNumEnd)
+  })
+
   const clipboardManager = container.get<TClipboardManager>(ClipboardManager)
   const statusBarManager = container.get<TStatusBarManager>(StatusBarManager)
-  await clipboardManager.copyToClipboard(symbols.join('\n\n'))
-  statusBarManager.updateCopyCount(symbols.length)
+  await clipboardManager.copyToClipboard(formattedSymbols.join('\n'))
+  statusBarManager.updateCopyCount(formattedSymbols.length)
 }
